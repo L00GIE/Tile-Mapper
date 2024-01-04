df4049fd9d4543db57bfb0fa875ff80a47d2fa34
L00GIE <l_ketelaars@hotmail.com>
2024-01-04 15:18:18 -0500

    Initial Commit

---
+++ Tile Mapper/Tile Mapper/Tile.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tile_Mapper
+{
+    public partial class Tile : UserControl
+    {
+        [JsonIgnore]
+        public Image image { get; set; }
+
+        public int rotation = 0;
+        public int imageIndex = 0;
+
+        public Tile()
+        {
+            InitializeComponent();
+        }
+
+        private void Tile_Load(object sender, EventArgs e)
+        {
+            if(this.image != null)
+                this.pictureBox1.Image = this.image;
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.BorderStyle = BorderStyle.None;
+        }
+
+        private void switchImage()
+        {
+            switch (this.imageIndex)
+            {
+                case 0: this.pictureBox1.Image = Tiles.grass; break;
+                case 1: this.pictureBox1.Image = Tiles.dirt; break;
+                case 2: this.pictureBox1.Image = Tiles.dirtedge; break;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.imageIndex += 1;
+                if (this.imageIndex >= Tiles.numtiles)
+                    this.imageIndex = 0;
+                switchImage();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                this.rotation += 90;
+                if (this.rotation > 270)
+                    this.rotation = 0;
+                Image rotoImage = pictureBox1.Image;
+                rotoImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                this.pictureBox1.Image = rotoImage;
+            }
+            this.pictureBox1.Update();
+        }
+    }
+}
---
+++ Tile Mapper/Tile Mapper/Tiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tile_Mapper.Properties;
+
+namespace Tile_Mapper
+{
+    public static class Tiles
+    {
+        public static int numtiles = 3;
+        public static Image grass = Resources.grass_16px;
+        public static Image dirt = Resources.dirt_16px;
+        public static Image dirtedge = Resources.dirt_edge_16px;
+    }
+}
